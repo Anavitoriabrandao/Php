a7c0dfa882a75c9c6796a3a9f79a87dabc454225
Ana Vitoria <anavitoriabrandaomarques@gmail.com>
2023-11-16 17:08:31 -0300

    versão 2 - modifiquei os prints

---
+++ exemplo2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<h2> exemplo 2 </h2>
+<?php
+
+$x = 5;
+$y = 4;
+echo"x=".$x;
+echo "<br> y=.$x";
+echo "<br> x+y= ".$x + $y;
+
+
+?>
+
+</body>
+</html>
